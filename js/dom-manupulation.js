75d9de5bc2040ccd8d6bd5efe0a567a67d137c01
dhiransapkota45 <dhiransapkota55@gmail.com>
2024-03-14 10:44:25 +0545

    changes

--- js/dom-manupulation.js
+++ js/dom-manupulation.js
@@ -24,3 +24,27 @@ console.log(divelement)
 
 // const data3 = divelement.innerText
 // console.log(data3)
+
+const namelement = document.getElementById('name');
+console.log(namelement.textContent)
+
+
+const btn = document.getElementById('btn');
+// let count = 0
+btn.addEventListener("click", () => {
+    // count++
+    // console.log("Button Clicked")
+    // namelement.textContent = "Hello World"
+    // namelement.style.color = "red"
+    // namelement.style.backgroundColor = "black"
+    // namelement.style.fontSize = "30px"
+    // namelement.style.fontFamily = "Arial"
+
+    // namelement.id = "newid"
+    // console.log(namelement.textContent === "Hello World")
+    if (namelement.textContent === "Hello World") {
+        namelement.textContent = "Hi world"
+    } else {
+        namelement.textContent = "Hello World"
+    }
+})
